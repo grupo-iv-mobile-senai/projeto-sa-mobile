beea9ead47b78d6ee5edeb936336c7046e06d1c6
davi wilpert de souza <daviwilpertt@gmail.com>
2024-06-10 12:42:07 -0300

    dsd

--- comum/componentes/ItemLista.js
+++ comum/componentes/ItemLista.js
@@ -1,34 +1,62 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import CORES from '../../comum/constantes/cores';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 
 const estilos = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row',
     backgroundColor: CORES.FUNDO_CARD,
     margin: 5,
     padding: 10,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: 'center',
+    width: 300,
   },
   texto: {
     fontSize: 20,
+    marginLeft: 10,
+  },
+  icone: {
+    marginRight: 10,
+  },
+  detalhesContainer: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
   }
 });
 
 const ItemLista = (props) => {
-  console.log(props);
-
   const detalhesVaga = () => {
-    
-  }
+    // Implementar navegação ou ação para mostrar detalhes da vaga
+  };
+
+  const buscarIcone = (veiculo) => {
+    switch (veiculo) {
+      case 'moto':
+        return 'motorbike';
+      case 'carro':
+        return 'car';
+      case 'van':
+        return 'van-passenger';
+      case 'caminhao':
+        return 'truck';
+      default:
+        return 'help-circle';
+    }
+  };
 
   return (
-    <TouchableOpacity onPress={detalhesVaga}>
+    <Pressable onPress={detalhesVaga}>
       <View style={estilos.container}>
-        <Text style={estilos.texto}>{props.item.nome_vaga}</Text>
-        <Text style={estilos.texto}>Endereço:{props.item.logradouro}</Text>
-        <Text style={estilos.texto}>Valor: R${props.item.valor}</Text>
+        <Icon name={buscarIcone(props.item.veiculo)} size={50} color="black" style={estilos.icone} />
+        <View style={estilos.detalhesContainer}>
+          <Text style={estilos.texto}>{props.item.nome_vaga}</Text>
+          <Text style={estilos.texto}>Endereço: {props.item.logradouro}</Text>
+          <Text style={estilos.texto}>Valor: R${props.item.valor}</Text>
+        </View>
       </View>
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
--- telas/TelaAnuncioVaga.js
+++ telas/TelaAnuncioVaga.js
@@ -5,9 +5,33 @@ import TELAS from "../comum/constantes/telas"
 import BotaoCustomizado from "../comum/componentes/BotaoCustomizado/BotaoCustomizado"
 import CORES from "../comum/constantes/cores"
 import api from "../comum/servicos/api"
+import RNPickerSelect from 'react-native-picker-select';
 
 
 const TelaAnuncioVaga = (props) => {
+
+    const pickerSelectStyles = StyleSheet.create({
+        inputIOS: {
+            fontSize: 16,
+            paddingVertical: 12,
+            paddingHorizontal: 10,
+            borderWidth: 1,
+            borderColor: 'gray',
+            borderRadius: 4,
+            color: 'black',
+            paddingRight: 30,
+        },
+        inputAndroid: {
+            fontSize: 16,
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderWidth: 0.5,
+            borderColor: 'purple',
+            borderRadius: 8,
+            color: 'black',
+            paddingRight: 30,
+        },
+    })
 
     const estilos = StyleSheet.create({
         tudo: {
@@ -29,6 +53,12 @@ const TelaAnuncioVaga = (props) => {
             alignItems: 'center',
             backgroundColor: CORES.FUNDO_ESCURO,
             borderRadius: 30
+        },
+        viewSelect: {
+            padding: 5,
+            width: 330,
+            margin: 5,
+            fontSize: 22,
         }
 
     })
@@ -36,6 +66,7 @@ const TelaAnuncioVaga = (props) => {
     const [nomeVaga, setNomeVaga] = useState('')
     const [capacidade, setCapacidade] = useState('')
     const [valor, setValor] = useState('')
+    const [veiculo, setVeiculo] = useState('')
     const [logradouro, setLogradouro] = useState('')
     const [bairro, setBairro] = useState('')
     const [cidade, setCidade] = useState('')
@@ -47,6 +78,7 @@ const TelaAnuncioVaga = (props) => {
                 nome_vaga: nomeVaga,
                 capacidade: capacidade,
                 valor: valor,
+                veiculo: veiculo,
                 logradouro: logradouro,
                 bairro: bairro,
                 cidade: cidade,
@@ -62,6 +94,7 @@ const TelaAnuncioVaga = (props) => {
         }
     }
 
+    
 
     return (
         <ScrollView style={estilos.tudo}>
@@ -71,6 +104,18 @@ const TelaAnuncioVaga = (props) => {
                 <CampoTextoCustomizado style={estilos.input} label='nome da vaga' value={nomeVaga} onChangeText={setNomeVaga} />
                 <CampoTextoCustomizado style={estilos.input} label='capacidade' value={capacidade} onChangeText={setCapacidade} />
                 <CampoTextoCustomizado style={estilos.input} label='valor' value={valor} onChangeText={setValor} />
+                <View style={estilos.viewSelect}>
+                    <RNPickerSelect style={pickerSelectStyles}
+                        onValueChange={setVeiculo}
+                        value={veiculo}
+                        items={[
+                            { label: 'moto', value: 'moto' },
+                            { label: 'carro', value: 'carro' },
+                            { label: 'van', value: 'van' },
+                            { label: 'caminhão', value: 'caminhao' },
+                        ]}
+                        placeholder={{ label: 'selecione uma operação', value: null }} />
+                </View>
                 <Text style={estilos.texto}>Endereço</Text>
                 <CampoTextoCustomizado style={estilos.input} label='logradouro' value={logradouro} onChangeText={setLogradouro} />
                 <CampoTextoCustomizado style={estilos.input} label='bairro' value={bairro} onChangeText={setBairro} />
